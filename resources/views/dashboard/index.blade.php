629b95b13aa3bbfdd147319ee721d4456654266f
Leonardo Geja <bemmenos@gmail.com>
2024-06-07 19:26:32 -0300

    create style dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,7 @@
-<h2>dashboard</h2>
+@extends('dashboard.master.app')
+
+@section('content')
+    <section class="flex container mx-auto h-screen w-full justify-center items-center p-2 md:p-0">
+        dashboard
+    </section>
+@endsection
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,4 +1,4 @@
-@extends('app')
+@extends('master.app')
 
 @section('content')
     <section class="flex container mx-auto h-screen w-full justify-center items-center p-2 md:p-0">
